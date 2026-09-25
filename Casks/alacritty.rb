8259e0f80b5d6d6e926b3445bf95dc16a6b81e5f
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2026-09-25 12:45:42 +0200

    fix(casks): declare alacritty as macos-only

--- Casks/alacritty.rb
+++ Casks/alacritty.rb
@@ -12,7 +12,7 @@ cask "alacritty" do
     strategy :github_latest
   end
 
-  depends_on macos: :macos
+  depends_on :macos
 
   app "Alacritty.app"
   binary "#{appdir}/Alacritty.app/Contents/MacOS/alacritty"
